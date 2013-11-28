1ce9f83633bd2357cec1791427b68dc82de347be
Marco Singer <markaum@gmail.com>
2013-11-28 14:29:23 -0200

    changing the package. Now is azure

--- blob.go
+++ blob.go
@@ -1,4 +1,4 @@
-package blob
+package azure
 
 import (
   "github.com/loldesign/azure/core"
@@ -13,51 +13,55 @@ import (
 
 var client = &http.Client{}
 
-type Azure struct {
-  Account string
-  AccessKey string
+type azure struct {
+  account string
+  key string
 }
 
-func (azure Azure) doRequest(azureRequest core.AzureRequest) (*http.Response, error) {
-  client, req := azure.clientAndRequest(azureRequest)
+func (a azure) doRequest(azureRequest core.AzureRequest) (*http.Response, error) {
+  client, req := a.clientAndRequest(azureRequest)
   return client.Do(req)
 }
 
-func (azure Azure) clientAndRequest(azureRequest core.AzureRequest) (*http.Client, *http.Request) {
-  req := azure.prepareRequest(azureRequest)
+func (a azure) clientAndRequest(azureRequest core.AzureRequest) (*http.Client, *http.Request) {
+  req := a.prepareRequest(azureRequest)
 
   return client, req
 }
 
-func (azure Azure) prepareRequest(azureRequest core.AzureRequest) *http.Request {
+func (a azure) prepareRequest(azureRequest core.AzureRequest) *http.Request {
   credentials := core.Credentials{
-    Account: azure.Account,
-    AccessKey: azure.AccessKey}
+    Account: a.account,
+    AccessKey: a.key}
 
   return core.New(credentials, azureRequest).PrepareRequest()
 }
 
-func (azure Azure) CreateContainer(container string) (*http.Response, error) {
+func New(account, accessKey string) azure {
+  return azure{account, accessKey}
+}
+
+func (a azure) CreateContainer(container string) (*http.Response, error) {
   azureRequest := core.AzureRequest{
     Method: "put",
     Container: container,
     Resource: "?restype=container",
     RequestTime: time.Now().UTC()}
 
-  return azure.doRequest(azureRequest)
+  return a.doRequest(azureRequest)
 }
 
-func (azure Azure) DeleteContainer(container string) (*http.Response, error) {
+func (a azure) DeleteContainer(container string) (*http.Response, error) {
   azureRequest := core.AzureRequest{
     Method: "delete",
     Container: container,
     Resource: "?restype=container",
     RequestTime: time.Now().UTC()}
 
-  return azure.doRequest(azureRequest)
+  return a.doRequest(azureRequest)
 }
 
-func (azure Azure) FileUpload(container, name string, file *os.File) (*http.Response, error) {
+func (a azure) FileUpload(container, name string, file *os.File) (*http.Response, error) {
   extension := strings.ToLower(path.Ext(file.Name()))
   contentType := mime.TypeByExtension(extension)
 
@@ -68,5 +72,5 @@ func (azure Azure) FileUpload(container, name string, file *os.File) (*http.Resp
     Header: map[string]string{"x-ms-blob-type": "BlockBlob", "Accept-Charset": "UTF-8", "Content-Type": contentType},
     RequestTime: time.Now().UTC()}
 
-  return azure.doRequest(azureRequest)
+  return a.doRequest(azureRequest)
 }
