8df76b77922625d5f300ebe8de3f6bf214d9855b
Marco Singer <markaum@gmail.com>
2013-12-23 21:17:00 -0200

    using gofmt and exporting the Azure struct

--- blob.go
+++ blob.go
@@ -1,129 +1,129 @@
 package azure
 
 import (
-  "github.com/loldesign/azure/core"
-  "net/http"
-  "fmt"
-  "time"
-  "os"
-  "mime"
-  "strings"
-  "path"
-  "encoding/xml"
+	"encoding/xml"
+	"fmt"
+	"github.com/loldesign/azure/core"
+	"mime"
+	"net/http"
+	"os"
+	"path"
+	"strings"
+	"time"
 )
 
 var client = &http.Client{}
 
-type azure struct {
-  account string
-  key string
+type Azure struct {
+	Account string
+	Key     string
 }
 
 type Blobs struct {
-  XMLName xml.Name `xml:"EnumerationResults"`
-  Itens []Blob `xml:"Blobs>Blob"`
+	XMLName xml.Name `xml:"EnumerationResults"`
+	Itens   []Blob   `xml:"Blobs>Blob"`
 }
 
 type Blob struct {
-  Name string `xml:"Name"`
-  Properties []Property `xml:"Properties"`
+	Name     string   `xml:"Name"`
+	Property Property `xml:"Properties"`
 }
 
 type Property struct {
-  LastModified string `xml:"Last-Modified"`
-  Etag string `xml:"Etag"`
-  ContentLength string `xml:"Content-Length"`
-  ContentType string `xml:"Content-Type"`
-  BlobType string `xml:"BlobType"`
-  LeaseStatus string `xml:"LeaseStatus"`
+	LastModified  string `xml:"Last-Modified"`
+	Etag          string `xml:"Etag"`
+	ContentLength string `xml:"Content-Length"`
+	ContentType   string `xml:"Content-Type"`
+	BlobType      string `xml:"BlobType"`
+	LeaseStatus   string `xml:"LeaseStatus"`
 }
 
-func (a azure) doRequest(azureRequest core.AzureRequest) (*http.Response, error) {
-  client, req := a.clientAndRequest(azureRequest)
-  return client.Do(req)
+func (a Azure) doRequest(azureRequest core.AzureRequest) (*http.Response, error) {
+	client, req := a.clientAndRequest(azureRequest)
+	return client.Do(req)
 }
 
-func (a azure) clientAndRequest(azureRequest core.AzureRequest) (*http.Client, *http.Request) {
-  req := a.prepareRequest(azureRequest)
+func (a Azure) clientAndRequest(azureRequest core.AzureRequest) (*http.Client, *http.Request) {
+	req := a.prepareRequest(azureRequest)
 
-  return client, req
+	return client, req
 }
 
-func (a azure) prepareRequest(azureRequest core.AzureRequest) *http.Request {
-  credentials := core.Credentials{
-    Account: a.account,
-    AccessKey: a.key}
+func (a Azure) prepareRequest(azureRequest core.AzureRequest) *http.Request {
+	credentials := core.Credentials{
+		Account:   a.Account,
+		AccessKey: a.Key}
 
-  return core.New(credentials, azureRequest).PrepareRequest()
+	return core.New(credentials, azureRequest).PrepareRequest()
 }
 
 func prepareMetadata(keys map[string]string) map[string]string {
-  header := make(map[string]string)
+	header := make(map[string]string)
 
-  for k, v := range keys {
-    key := fmt.Sprintf("x-ms-meta-%s", k)
-    header[key] = v
-  }
+	for k, v := range keys {
+		key := fmt.Sprintf("x-ms-meta-%s", k)
+		header[key] = v
+	}
 
-  return header
+	return header
 }
 
-func New(account, accessKey string) azure {
-  return azure{account, accessKey}
+func New(account, accessKey string) Azure {
+	return Azure{account, accessKey}
 }
 
-func (a azure) CreateContainer(container string, meta map[string]string) (*http.Response, error) {
-  azureRequest := core.AzureRequest{
-    Method: "put",
-    Container: container,
-    Resource: "?restype=container",
-    Header: prepareMetadata(meta),
-    RequestTime: time.Now().UTC()}
+func (a Azure) CreateContainer(container string, meta map[string]string) (*http.Response, error) {
+	azureRequest := core.AzureRequest{
+		Method:      "put",
+		Container:   container,
+		Resource:    "?restype=container",
+		Header:      prepareMetadata(meta),
+		RequestTime: time.Now().UTC()}
 
-  return a.doRequest(azureRequest)
+	return a.doRequest(azureRequest)
 }
 
-func (a azure) DeleteContainer(container string) (*http.Response, error) {
-  azureRequest := core.AzureRequest{
-    Method: "delete",
-    Container: container,
-    Resource: "?restype=container",
-    RequestTime: time.Now().UTC()}
+func (a Azure) DeleteContainer(container string) (*http.Response, error) {
+	azureRequest := core.AzureRequest{
+		Method:      "delete",
+		Container:   container,
+		Resource:    "?restype=container",
+		RequestTime: time.Now().UTC()}
 
-  return a.doRequest(azureRequest)
+	return a.doRequest(azureRequest)
 }
 
-func (a azure) FileUpload(container, name string, file *os.File) (*http.Response, error) {
-  extension := strings.ToLower(path.Ext(file.Name()))
-  contentType := mime.TypeByExtension(extension)
+func (a Azure) FileUpload(container, name string, file *os.File) (*http.Response, error) {
+	extension := strings.ToLower(path.Ext(file.Name()))
+	contentType := mime.TypeByExtension(extension)
 
-  azureRequest := core.AzureRequest{
-    Method: "put",
-    Container: fmt.Sprintf("%s/%s", container, name),
-    Body: file,
-    Header: map[string]string{"x-ms-blob-type": "BlockBlob", "Accept-Charset": "UTF-8", "Content-Type": contentType},
-    RequestTime: time.Now().UTC()}
+	azureRequest := core.AzureRequest{
+		Method:      "put",
+		Container:   fmt.Sprintf("%s/%s", container, name),
+		Body:        file,
+		Header:      map[string]string{"x-ms-blob-type": "BlockBlob", "Accept-Charset": "UTF-8", "Content-Type": contentType},
+		RequestTime: time.Now().UTC()}
 
-  return a.doRequest(azureRequest)
+	return a.doRequest(azureRequest)
 }
 
-func (a azure) ListBlobs(container string) (Blobs, error) {
-  var blobs Blobs
+func (a Azure) ListBlobs(container string) (Blobs, error) {
+	var blobs Blobs
 
-  azureRequest := core.AzureRequest{
-    Method: "get",
-    Container: container,
-    Resource: "?restype=container&comp=list",
-    RequestTime: time.Now().UTC()}
+	azureRequest := core.AzureRequest{
+		Method:      "get",
+		Container:   container,
+		Resource:    "?restype=container&comp=list",
+		RequestTime: time.Now().UTC()}
 
-  res, err := a.doRequest(azureRequest)
+	res, err := a.doRequest(azureRequest)
 
-  if err != nil {
-    return blobs, err
-  }
+	if err != nil {
+		return blobs, err
+	}
 
-  decoder := xml.NewDecoder(res.Body)
-  decoder.Decode(&blobs)
+	decoder := xml.NewDecoder(res.Body)
+	decoder.Decode(&blobs)
 
-  return blobs, nil
+	return blobs, nil
 }
